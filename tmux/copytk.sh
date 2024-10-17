b1d1d961bbfce2d952601ca434e9fe05ace17e6b
Joseph Harriott <trohib@gmail.com>
2024-10-17 12:06:38 +0200

    updated  fdtsExt

--- tmux/copytk.sh
+++ tmux/copytk.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 # vim: fdl=1 tw=1:
 
-# Joseph Harriott - https://harriott.github.io/ - Tue 17 Sep 2024
+# Joseph Harriott - https://harriott.github.io/ - Mon 23 Sep 2024
 
 # on Arch need to  chmod u+x $OSL/tmux/copytk.sh
 # run'd  from  $OSL/tmux/tmux.conf
 
 #=> for tmux-copy-toolkit
 # uname -r > $OSL/tmux/unamer  # checks
-if [[ $(uname -r) =~ 'arch' ]]; then
+ur=$(uname -r)
+if [[ $ur =~ 'arch' || $ur =~ 'zen' ]]; then
     tmux set -g @copytk-copy-command 'tmux loadb - && tmux showb | copyq add -'
     # - only gets into X selection once pasted from  CopyQ
 else
