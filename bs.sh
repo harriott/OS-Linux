e1f3684ec57f9ac042b996692cd31280e2b6b4b7
Joseph Harriott <trohib@gmail.com>
2025-01-05 20:58:59 +0100

    nmsh on WSL Ubuntu

---
+++ bs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# vim: fdl=1 sw=2:
+
+# bash $OSL/bs.sh
+
+set -e
+
+# #=> CLM - $maild
+# mkdir $maild $maild/cz $maild/fm $maild/tj $maild/live $maild/troh $maild/gmx $maild/zou
+
+# #=> CLM - configs
+# cp -f $lclm/mbsyncrc-backup ~/.mbsyncrc
+# cp -f $lclm/msmtprc-backup ~/.msmtprc; chmod 600 ~/.msmtprc
+# cp -f $lclm/notmuch-config-backup ~/.notmuch-config
+
