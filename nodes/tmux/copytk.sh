286451e7a15d375509ca87c1e69b64325e5c7992
Joseph Harriott <trohib@gmail.com>
2025-01-05 22:12:46 +0100

    gvc

--- nodes/tmux/copytk.sh
+++ nodes/tmux/copytk.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 # vim: fdl=1 tw=1:
 
-# Joseph Harriott - https://harriott.github.io/ - Mon 23 Sep 2024
+# dim 05 janv 2025
 
-# on Arch need to  chmod u+x $OSL/tmux/copytk.sh
-# run'd  from  $OSL/tmux/tmux.conf
+# on Arch need to  chmod u+x $OSL/nodes/tmux/copytk.sh
+# run'd  from  $OSL/nodes/tmux/tmux.conf
 
 #=> for tmux-copy-toolkit
 # uname -r > $OSL/tmux/unamer  # checks
-ur=$(uname -r)
-if [[ $ur =~ 'arch' || $ur =~ 'zen' ]]; then
+ur=$(uname -r); if [[ $ur =~ 'arch' || $ur =~ 'zen' ]]; then
     tmux set -g @copytk-copy-command 'tmux loadb - && tmux showb | copyq add -'
     # - only gets into X selection once pasted from  CopyQ
-else
+else  # microsoft WSL
     tmux set -g @copytk-copy-command 'tmux loadb - && tmux showb | clip.exe'
     # - arrives in  Ditto
 fi
